9a391be59a70390b23d0e6b5fc79f77058f6ee3b
Chris Conner <chris.conner@gmail.com>
2017-09-06 17:55:10 -0500

    Initial Commit

---
+++ HACodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HACodingChallenge
+//
+//  Created by Chris Conner on 9/6/17.
+//  Copyright © 2017 Chris Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
